a941686c6e001324fde17393ee349642dddd72b4
Tara <taconlin@hotmail.co.uk>
2024-07-31 15:18:56 +0100

    add react bootstrap to post component

--- frontend/src/components/Post/Post.jsx
+++ frontend/src/components/Post/Post.jsx
@@ -1,5 +1,19 @@
+import Toast from 'react-bootstrap/Toast';
+
 const Post = (props) => {
-  return <article key={props.post._id}>{props.post.message}</article>;
+  return (
+  <div className="d-flex justify-content-center">
+    <Toast style = {{width: "60%"}}>
+    <Toast.Header>
+    <img src="holder.js/20x20?text=%20" className="rounded me-2" alt="" />
+    <strong className="me-auto">*Insert Username*</strong>
+    <small>Posted on {props.post.message.substr(-20)}</small>
+    </Toast.Header>
+    <Toast.Body><article key={props.post._id}>{props.post.message}</article><br></br><img src={props.filepath}style={{
+          width: "80%"}} ></img></Toast.Body><br></br>
+</Toast>
+</div>
+);
 };
 
 export default Post;
