626586d8d15f9e80dc2313ed1ed797b151eb8745
abdallah <aharundd@gmail.com>
2024-07-31 11:52:46 +0100

    adding bcrypt to users and authentication in controllers

--- api/controllers/authentication.js
+++ api/controllers/authentication.js
@@ -1,25 +1,36 @@
 const User = require("../models/user");
 const { generateToken } = require("../lib/token");
+const bcrypt = require("bcrypt");
 
 const createToken = async (req, res) => {
-  const email = req.body.email;
-  const password = req.body.password;
+    const email = req.body.email;
+    const password = req.body.password;
 
-  const user = await User.findOne({ email: email });
-  if (!user) {
-    console.log("Auth Error: User not found");
-    res.status(401).json({ message: "User not found" });
-  } else if (user.password !== password) {
-    console.log("Auth Error: Passwords do not match");
-    res.status(401).json({ message: "Password incorrect" });
-  } else {
-    const token = generateToken(user.id);
-    res.status(201).json({ token: token, message: "OK" });
-  }
+    const user = await User.findOne({ email: email });
+    if (!user) {
+        console.log("Auth Error: User not found");
+        res.status(401).json({ message: "User not found" });
+    } else {
+        // bcrypt compare - Abdallah
+        bcrypt.compare(password, user.password, (err, result) => {
+            if (err) {
+                console.error(err);
+                res.status(400).json({
+                    message: "Unable to compare passwords",
+                });
+            } else if (!result) {
+                console.log("Auth Error: Passwords do not match");
+                res.status(401).json({ message: "Password incorrect" });
+            } else {
+                const token = generateToken(user.id);
+                res.status(201).json({ token: token, message: "OK" });
+            }
+        });
+    }
 };
 
 const AuthenticationController = {
-  createToken: createToken,
+    createToken: createToken,
 };
 
 module.exports = AuthenticationController;
