67343f3283169e6b66cc81483936beab7377e061
abdallah <aharundd@gmail.com>
2024-08-06 10:10:31 +0100

    adding delete controller and route

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -6,5 +6,6 @@ const PostsController = require("../controllers/posts");
 router.get("/all", PostsController.getAllPosts);
 router.post("/", PostsController.createPost);
 router.get("/user", PostsController.getUserPosts);
+router.delete("/:id", PostsController.deletePost);
 
 module.exports = router;
