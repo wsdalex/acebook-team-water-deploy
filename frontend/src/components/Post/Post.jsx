037ac0de1a3da065a9d395459e7f48465e63212d
Karina Dawson <karinajdawson@gmail.com>
2024-08-07 15:18:43 +0100

    drop down for comments complete and altered seed data to accept comments

--- frontend/src/components/Post/Post.jsx
+++ frontend/src/components/Post/Post.jsx
@@ -5,6 +5,7 @@ import { useNavigate } from "react-router-dom";
 import { FaHeart, FaRegHeart } from "react-icons/fa";
 import { useState } from "react";
 import { likePost } from "../../services/posts";
+import Accordion from "react-bootstrap/Accordion";
 
 
 const Post = (props) => {
@@ -36,54 +37,79 @@ const Post = (props) => {
     const image = props.post.imageUrl;
     const comments = props.post.comments;
     return (
-        <div className='d-flex justify-content-center'>
-            <Toast style={{ width: "60%" }}>
-                <Toast.Header>
-                    <img
-                        src='holder.js/20x20?text=%20'
-                        className='rounded me-2'
-                        alt=''
-                    />
-                    <strong className='me-auto'>
-                        {userName ? userName : ""}
-                    </strong>{" "}
-                    {/* Added user details in posts controller in backend to be used here */}
-                    <Button variant='link' onClick={handleLike}>
-                        {liked ? <FaHeart color='red' data-testid="filled-heart-icon" /> : <FaRegHeart />}
-                        <span>{numberOfLikes}</span>
-                    </Button>
-                    <small>Posted {formattedDate}</small>
-                </Toast.Header>
-                <Toast.Body>
-                    <article key={props.post._id}>{props.post.message}</article>
-                    <br></br>
-                    <img
-                        src={image ? image : props.filepath}
-                        style={{
-                            width: "80%",
-                        }}
-                    ></img>
-                </Toast.Body>
-                <br></br>
-                <Button onClick={handleAddComment}>Add a comment</Button>
-                          <br />
-          <br /> 
-          <div className="comments-section">
-            <h5>Comments</h5>
-            {comments.length > 0 ? (
+        <div className='d-flex justify-content-center' style={{ marginBottom: "30px" }}>
+          <Toast style={{ width: "60%" }}>
+            <Toast.Header>
+              <img
+                src='holder.js/20x20?text=%20'
+                className='rounded me-2'
+                alt=''
+              />
+              <strong className='me-auto'>
+                {userName ? userName : ""}
+              </strong>{" "}
+              {/* Added user details in posts controller in backend to be used here */}
+              <Button variant='link' onClick={handleLike}>
+                {liked ? <FaHeart color='red' data-testid="filled-heart-icon" /> : <FaRegHeart />}
+                <span>{numberOfLikes}</span>
+              </Button>
+                <small>Posted {formattedDate}</small>
+            </Toast.Header>
+
+            <Toast.Body>
+              <article key={props.post._id}>{props.post.message}</article>
+              <br></br>
+              <img
+                src={image ? image : props.filepath}
+                style={{width: "80%",}}
+              ></img>
+            </Toast.Body>
+              <br></br>
+              <Button onClick={handleAddComment}>Add a comment</Button>
+              <br />
+              <br /> 
+      
+          <Accordion>
+          <Accordion.Item eventKey="0">
+            
+            <Accordion.Header>
+              <span style={{ fontSize: "0.9rem" }}>Comments:</span>
+            </Accordion.Header>
+            
+            <Accordion.Body>
+              {comments.length > 0 ? (
               comments.map((comment) => (
-                <div key={comment._id} className="comment">
-                  <p><strong>{comment.user_id.name}</strong>: {comment.comment}</p>
-                  </div>
+              <div key={comment._id} className="comment" style={commentStyle}>
+              <p>
+                <p><strong>{comment.user_id.name}</strong>: {comment.comment}</p>
+              </p> 
+                <hr style={separatorStyle} />
+              </div>
               ))
-            ) : (
+              ) : (
               <p>No comments yet. Be the first to comment!</p>
-            )}
-          </div>
-            </Toast>
+              )}
+            </Accordion.Body>
+
+          </Accordion.Item>
+
+          </Accordion>
+
+          </Toast>
+        
         </div>
     );
 
 };
-//line 37 is where comments added to post starts
+
+const commentStyle = {
+  textAlign: "left",
+  padding: "10px 0",
+};
+
+const separatorStyle = {
+  border: "1px solid #e0e0e0",
+  margin: "10px 0",
+};
+//line 72 is where comments added to post starts using Accordian as a bootstrap dropdown
 export default Post;
