79ed921e04b2553abefa89e0f16c777582bb83cd
abdallah <aharundd@gmail.com>
2024-08-07 13:49:07 +0100

    profile-image-in-signup-and-posts

--- frontend/src/components/Post/Post.jsx
+++ frontend/src/components/Post/Post.jsx
@@ -2,6 +2,7 @@ import Toast from 'react-bootstrap/Toast';
 import moment from 'moment';
 import Button from 'react-bootstrap/Button';
 import { useNavigate } from 'react-router-dom';
+import "./Post.css";
 
 const Post = (props) => {
 
@@ -13,14 +14,17 @@ const Post = (props) => {
 
   }
   const userName = props.post.user_id.name
+  const profileImage = props.post.user_id.profileImage
   const formattedDate = moment(props.post.createdAt).fromNow();
   const image = props.post.imageUrl;
+
   return (
   <div className="d-flex justify-content-center">
     <Toast style = {{width: "60%"}}>
     <Toast.Header>
     <img src="holder.js/20x20?text=%20" className="rounded me-2" alt="" />
 
+    <img src={profileImage ? profileImage : "holder.js/20x20?text=%20"} className="rounded me-2 profile-image" alt="Profile" />
     <strong className="me-auto">{userName ? userName : ""}</strong> {/* Added user details in posts controller in backend to be used here */}
     <small>Posted {formattedDate}</small>
 
